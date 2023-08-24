d4240f30e49d6f2fcc4b66a1ee7b0177f9dc9997
khrahat92 <khrahat92@gmail.com>
2023-08-25 00:30:06 +0600

    create a basic react context api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { UserProvider, useUser } from "./context/userContext";
+// import {BrowserRouter as Router } from 'react-router-dom';
 function App() {
+  const { user } = useUser();
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Context</h1>
+      <h2>user.name: {user.name}</h2>
+    </>
   );
 }
 
