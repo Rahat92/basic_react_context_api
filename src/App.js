c90d48dc6e86f22e34453363d01d07633c3a8c0c
khrahat92 <khrahat92@gmail.com>
2023-09-08 17:12:51 +0600

    react-toastify

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { UserProvider, useUser } from "./context/userContext";
-import { NumberContext, useNumber } from "./context/numberContext";
-import { useState } from "react";
-import { useContext } from "react";
-import MyNumber from "./MyNumber";
-// import {BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
-  const { user } = useUser();
-  const [myNumber, setMyNumber] = useState(0);
-  console.log(user);
-  console.log(myNumber);
+  const notify = () => toast("Wow so easy!");
   return (
-    <NumberContext.Provider value={{ number:myNumber }}>
-      <div>
-        <MyNumber />
-        <h1>React Context</h1>
-        <h2>user.name: {user.name}</h2>
-        <button
-          onClick={() => {
-            setMyNumber(myNumber + 1);
-          }}
-          style={{
-            padding: ".5rem",
-            background: "red",
-            borderRadius: ".3rem",
-            marginLeft: "1rem",
-            cursor: "pointer",
-          }}
-        >
-          +
-        </button>
-        <MyNumber />
-      </div>
-    </NumberContext.Provider>
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
   );
 }
 
--- src/context/numberContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-export const NumberContext = createContext({
-    number:0
-});
