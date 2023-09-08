797b2b709606a6b732549af75abf4e997f7a0c06
khrahat92 <khrahat92@gmail.com>
2023-09-08 17:53:36 +0600

    react-loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+import { Audio, FallingLines, TailSpin, Circles } from "react-loader-spinner";
+
 function App() {
-  const notify = () => toast("Wow so easy!");
-  return (
-    <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    setLoading(false);
+  }, []);
+  if (loading) {
+    return (
+      <TailSpin
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />
+    );
+  }
+  return <div>data fetched</div>;
 }
 
 export default App;
